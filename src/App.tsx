4088b7762ba78c69b0de344ad2e4dd8e87088bab
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-03-31 13:30:19 +0000

    Iinital commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  
+
+  return (
+    <h1 className="text-3xl text-center font-bold underline">
+       Hello Welcome to the student connect platform
+    </h1>
+  )
+}
+
+export default App
