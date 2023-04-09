944b910a00b9e3b23ef1a40e28d0a6b66beb0331
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-04-09 21:28:04 +0000

    Making some chages in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,8 +10,8 @@ const Navbar: React.FC<NavProps> = ({ logoUrl }) => {
 
   return (
     // =============================  main container  ================ 
-    <nav className="bg-gray-200">
-      <div className="max-w-7xl mx-auto px-2 md:px-6 lg:px-8">
+    <nav className="bg-white border  shadow-sm h-[4rem] z-50 ">
+      <div className="max-w-7xl mx-auto mt-2 px-2 md:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-10">
 
             {/* ====================  TOGGLE SECTION  ================== */}
@@ -85,25 +85,7 @@ const Navbar: React.FC<NavProps> = ({ logoUrl }) => {
                   Videos
                 </a>
 
-                <a
-                  href="#"
-                  className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-white hover:bg-gray-700"
-                >
-                  Podcasts
-                </a>
-                <a
-                  href="#"
-                  className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-white hover:bg-gray-700"
-                >
-                  Products
-                </a>
-
-                <a
-                  href="#"
-                  className="px-3 py-2 rounded-md text-sm font-medium text-gray-700 hover:text-white hover:bg-gray-700"
-                >
-                  Resources
-                </a>
+               
 
                 <a
                   href="#"
@@ -117,15 +99,21 @@ const Navbar: React.FC<NavProps> = ({ logoUrl }) => {
           {/* END OF THE NAVBAR IN THE DESKTOP VIEW  */}
 
           {/*=======================  SEARCH  BAR  ==========================   */}
-          <div className="flex items-center">
-            {/* Search icons for the search through content  */}
-              <input type="search" />
-            <div className="flex-shrink-0">
-              <button className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                <span className="sr-only">Search</span>
-               <BsSearch/>
-              </button>
-            </div>
+          <div className="flex items-center ">
+            
+<form className="flex items-center">   
+    <label className="sr-only">Search</label>
+    <div className="relative w-full">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+        </div>
+        <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search" required />
+    </div>
+    <button type="submit" className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        <span className="sr-only">Search</span>
+    </button>
+</form>
 
             {/* ACCOUNT  */}
             <div className="hidden sm:block sm:ml-6">
@@ -133,8 +121,8 @@ const Navbar: React.FC<NavProps> = ({ logoUrl }) => {
                 <button className="bg-white py-2 px-4 border border-transparent rounded-md text-sm font-medium text-gray-900 hover:bg-gray-50">
                   Log in
                 </button>
-                <button className="bg-white py-2 px-4 border border-transparent rounded-md text-sm font-medium text-gray-900 hover:bg-gray-50">
-                  Sign up
+                <button className="bg-white text-blue-800  border-blue-800 py-2 px-4 border border-transparent rounded-md text-sm font-medium  hover:bg-gray-50">
+                 Crate Account
                 </button>
               </div>
             </div>
@@ -143,11 +131,12 @@ const Navbar: React.FC<NavProps> = ({ logoUrl }) => {
       </div>
 
       {/* Mobile menu */}
+      <section className="z-50">
       <div
         className={`${isOpen ? "block" : "hidden"} md:hidden`}
         id="mobile-menu"
       >
-        <div className="px-2 pt-2 pb-3 space-y-1">
+        <div className="px-2 pt-2 pb-3 space-y-1 z-50">
           <a
             href="#"
             className="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900"
@@ -260,6 +249,7 @@ const Navbar: React.FC<NavProps> = ({ logoUrl }) => {
           {/* END OF THE PROFILE */}
         </div>
       </div>
+      </section>
     </nav>
   );
 };
