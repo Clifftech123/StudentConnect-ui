439137673053166a8e9008773eaa6302382f967a
Isaiah Clifford Opoku <opokuisaiahclifford123@gmail.com>
2023-04-11 23:26:53 +0000

    Adding  of pages

---
+++ src/pages/login.tsx
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+import {Facebook,GitHub,Google} from '@mui/icons-material'
+
+
+
+  const LoginForm = () => {
+    return(
+      
+  <>
+  
+  
+  </>
+ 
+  
+  )
+}
+
+export default  LoginForm
